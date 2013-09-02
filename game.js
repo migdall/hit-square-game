e04aa688049959be348f3725ded8ae6b228d0235
clowkun <konpah23@yahoo.com>
2013-09-02 12:21:00 -0700

    Using the Kinectic HTML5 canvas framework and starting the first tutorial.

--- game.js
+++ game.js
@@ -7,3 +7,26 @@
  */
 
 
+$(document).ready(function() {
+	var stage = new Kinetic.Stage({
+		container: 'gameBoard',
+		width: 640,
+		height: 320
+	});
+
+	var layer = new Kinetic.Layer();
+
+	var rect = new Kinetic.Rect({
+		x: 239,
+		y: 75,
+		width: 100,
+		height: 50,
+		fill: 'green',
+		stroke: 'black',
+		strokeWidth: 4
+	});
+
+	layer.add(rect);
+
+	stage.add(layer);
+});
